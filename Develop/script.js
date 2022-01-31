7d1e4726cbe8d43949e987a49a007e5c7d894bbc
maddnty <nzkb24@gmail.com>
2022-01-30 22:06:58 -0600

    console log times with am or pm

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,32 @@
 // formats current date and time
 const current = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-console.log(current);
 
 // displays current date and time 
 const rightNow = $('#rightNow');
-rightNow.addClass("d-flex row-md-8 justify-content-center");
+rightNow.addClass("d-flex row-md-8 justify-content-center mb-2");
 rightNow.append(current);
 
+// render time blocks
+const planner = $('#planner'); 
+planner.addClass('container')
+
+const boxTime = $('<span>');
+boxTime.attr('class', 'time-block');
+
+let currentHour = moment().hours();
+console.log(currentHour);
+
+for(let hour = 5; hour <= 20; hour++) {
+
+    let displayTime = 0;
+    let meridian = "";
+    if (hour > 12){
+        displayTime = hour - 12;
+        meridian = "pm";
+    }else{
+        displayTime = hour;
+        meridian = "am";
+    }
+
+console.log(displayTime, meridian);
+}
